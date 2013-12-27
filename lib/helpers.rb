e52c0ef8ebf1d98dcad581e350a7fc420f11549c
Eric Teubert <eric@ericteubert.de>
2013-12-27 22:37:03 +0100

    add more template references

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,13 +4,7 @@ include Nanoc::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Filtering
 
-# module Podlove
-
-	def templateRefClasses
-		[
-			JSON.parse(IO.read('./sources/template/episode.json')),
-			JSON.parse(IO.read('./sources/template/chapter.json')),
-		]
-	end
-
-# end
+def templateRefClasses
+	classes = %w{asset episode file license chapter feed file_type podcast}
+	classes.map { |klass| JSON.parse(IO.read("./sources/template/#{klass}.json")) }
+end
