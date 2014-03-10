bf435d70047f0740027123f79695512f2289a9f1
Eric Teubert <eric@ericteubert.de>
2014-03-10 15:23:18 +0530

    update template reference

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,6 +5,6 @@ include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Filtering
 
 def templateRefClasses
-	classes = %w{podcast episode chapter feed asset file file_type contributor license}
+	classes = %w{podcast episode chapter feed asset file file_type contributor contributor_group service license}
 	classes.map { |klass| JSON.parse(IO.read("./sources/template/#{klass}.json")) }
 end
