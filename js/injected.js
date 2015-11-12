528ea098188a443b36229ce1fa948f6976c0ce9b
plutonik <plutonik@gmx.net>
2015-11-12 23:06:25 +0100

    Add script for injected data demo

--- js/injected.js
+++ js/injected.js
@@ -6,25 +6,25 @@ var pwp_metadata = {};
 pwp_metadata['inject'] = {
     sources: [
         {
-            src: "{{site.dist}}/examples/which-format/podlove-test-track.mp4",
+            src: "/bower_components/podlove-web-player/dist/examples/which-format/podlove-test-track.mp4",
             type: "audio/mp4"
         },
         {
-            src:"{{site.dist}}/examples/which-format/podlove-test-track.mp3",
+            src:"/bower_components/podlove-web-player/dist/examples/which-format/podlove-test-track.mp3",
             type:"audio/mpeg"
         },
         {
-            src:"{{site.dist}}/examples/which-format/podlove-test-track.ogg",
+            src:"/bower_components/podlove-web-player/dist/examples/which-format/podlove-test-track.ogg",
             type:"audio/ogg; codecs=vorbis"
         },
         {
-            src:"{{site.dist}}/examples/which-format/podlove-test-track.opus",
+            src:"/bower_components/podlove-web-player/dist/examples/which-format/podlove-test-track.opus",
             type:"audio/ogg; codecs=opus"
         }
     ],
-    poster: '{{site.dist}}/examples/coverimage.png',
+    poster: '/bower_components/podlove-web-player/dist/examples/coverimage.png',
     title: 'PWP001 - Which format plays?',
-    permalink: '{{site.dist}}/examples/which-format/index.html',
+    permalink: '/bower_components/podlove-web-player/dist/examples/which-format/index.html',
     subtitle: 'The Format your client chooses to play first will be told when you play this track.',
     publicationDate: '2004-02-12T15:19:21+00:00',
     "license": {
@@ -39,12 +39,12 @@ pwp_metadata['inject'] = {
         {
             start: '00:00:00.500',
             title: 'Chapter Two',
-            image: '{{site.dist}}/examples/coverimage-red.png'
+            image: '/bower_components/podlove-web-player/dist/examples/coverimage-red.png'
         },
         {
             start: '00:00:01.500',
             title: 'Chapter Three',
-            image: '{{site.dist}}/examples/coverimage-green.png',
+            image: '/bower_components/podlove-web-player/dist/examples/coverimage-green.png',
             href: 'http://wordpress.org/plugins/podlove-podcasting-plugin-for-wordpress/'
         },
         {
@@ -56,30 +56,30 @@ pwp_metadata['inject'] = {
     summary: '<p>Summary and even links <a href="https://github.com/podlove/podlove-web-player">Podlove Web Player</a>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna.</p><p>Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras mattis consectetur purus sit amet fermentum. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>',
     downloads: [{
         name: 'MP3', size: 58725,
-        url: '{{site.dist}}/example/which-format/podlove-test-track.mp3',
-        dlurl: '{{site.dist}}/example/which-format/podlove-test-track.mp3'
+        url: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.mp3',
+        dlurl: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.mp3'
     }, {
         name: 'ogg', size: 50494,
-        url: '{{site.dist}}/example/which-format/podlove-test-track.ogg',
-        dlurl: '{{site.dist}}/example/which-format/podlove-test-track.mp3'
+        url: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.ogg',
+        dlurl: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.mp3'
     }, {
         name: 'MP4', size: 78328,
-        url: '{{site.dist}}/example/which-format/podlove-test-track.mp4',
-        dlurl: '{{site.dist}}/example/which-format/podlove-test-track.mp4'
+        url: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.mp4',
+        dlurl: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.mp4'
     }, {
         name: 'opus', size: 37314,
-        url: '{{site.dist}}/examples/which-format/podlove-test-track.opus',
-        dlurl: '{{site.dist}}/example/which-format/podlove-test-track.opus'
+        url: '/bower_components/podlove-web-player/dist/examples/which-format/podlove-test-track.opus',
+        dlurl: '/bower_components/podlove-web-player/dist/example/which-format/podlove-test-track.opus'
     }],
     show: {
         title: 'PWP - The Podlove Web Player',
         subtitle: 'HTML5 Goodness for Podcasts',
         summary: 'Even more text about this player and its advantages...',
-        poster: '{{site.dist}}/examples/coverimage.png',
+        poster: '/bower_components/podlove-web-player/dist/examples/coverimage.png',
         url: 'http://docs.podlove.org'
     },
     duration: '00:02.902',
     alwaysShowHours: true,
     width: 'auto'
 };
-$('audio').podlovewebplayer({staticEmbedPage:'{{site.dist}}/static.html'});
+$('audio').podlovewebplayer({staticEmbedPage:'/bower_components/podlove-web-player/dist/static.html'});
